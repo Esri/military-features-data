f1d46c76eb94d9031f9036ce3479346ea0ff4780
Andy Bouffard <abouffard@esri.com>
2015-10-05 15:10:32 -0500

    New tools to support the creation of mil feat geodatabase template
    Two GP tools added to a new toolbox.
    CreateTemplateGDB reads a pair of CSV files to create the core of a new
    geodatabase.  It then calls the  AddFieldsFromSchema tool to add
    specified fields to each feature class in that geodatabase.
    
    Note: there appears to be a bug in the latest 1.1.1 update of Pro.
    Toolboxes created in the Project tab cannot be renamed with spaces in
    the Label.  One would expect that the Name of the toolbox should be
    spaceless and become the name of the tbx file.  One would expect the
    Label property to be the property that drives how the toolbox is listed
    in the Project tab, and therefore allow spaces.  This no longer appears
    to be the case.

---
+++ data/mil2525d/utilities/military-features-utilities/template-gdb-toolbox/scripts/CreateTemplateGDB/AddFieldsFromSchema.py
@@ -0,0 +1,120 @@
+# ----------------------------------------------------------------------------------
+# Copyright 2013-2015 Esri
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+# AddFieldsFromSchema.py
+# Description: Reads the field descriptions from a field schema CSV file and
+# adds those fields to the specified feature class.
+# Requirements: ArcGIS Desktop
+# ----------------------------------------------------------------------------------
+
+import arcpy, os, sys, traceback
+import csv as csv
+
+from arcpy import env
+from os import path
+
+def fieldTypeLookup(jmsmlFieldType):
+    # Converts a JMSML field type into an Esri field type
+    switcher = {
+                "Date": "DATE",
+                "Double": "DOUBLE",
+                "Integer": "LONG",
+                "SmallInteger": "SHORT",
+                "String": "TEXT",
+                }
+    
+    return switcher.get(jmsmlFieldType, "NONE")
+
+def castValue(valueType, originalValue):
+    # Cast input string value to a specified type
+    
+    if valueType == "DOUBLE":
+        castedValue = float(originalValue)
+    elif valueType == "LONG":
+        castedValue = int(originalValue)
+    elif valueType == "SHORT":
+        castedValue = int(originalValue)
+    else:
+        castedValue = originalValue
+        
+    return castedValue
+    
+def addFieldsFromSchema(featureClass="GET_PARAMETER", schema="GET_PARAMETER"):
+    
+    # Adds the fields defined in a field schema CSV file to the specified feature class
+    
+    try:
+        arcpy.AddMessage("Starting: AddFieldsFromSchema")
+        
+        currentPath = os.path.dirname(__file__)
+        defaultDataPath = os.path.normpath(os.path.join(currentPath, r'../../tooldata/'))
+    
+        if featureClass == "GET_PARAMETER":
+            featureClass = arcpy.GetParameterAsText(0)
+            
+        if(schema == "GET_PARAMETER"):
+            schema = arcpy.GetParameterAsText(1)
+            
+        # Make sure the feature class exists
+        
+        if arcpy.Exists(featureClass):
+            
+            # Make sure the specfied field schema CSV file exists
+            
+            fieldSchemaFile = os.path.join(defaultDataPath, "Fields_" + schema + ".csv")
+            if os.path.exists(fieldSchemaFile):
+                with open(fieldSchemaFile, 'r') as csvFile:
+                    reader = csv.reader(csvFile, dialect='excel')
+            
+                    # Skip the headers
+                    header = next(reader)
+                    
+                    # Read all the rows and add fields accordingly
+                    for line in reader:
+                        
+                        fieldType = fieldTypeLookup(line[1])
+                        
+                        if fieldType == "TEXT":
+                            length = int(line[2])
+                            
+                            
+                            arcpy.AddField_management(featureClass, \
+                                                      field_name=line[0], \
+                                                      field_type=fieldType, \
+                                                      field_length=length, \
+                                                      field_alias=line[3], \
+                                                      field_is_nullable=line[4])
+                        else:
+                            arcpy.AddField_management(featureClass, \
+                                                      field_name=line[0], \
+                                                      field_type=fieldType, \
+                                                      field_alias=line[3], \
+                                                      field_is_nullable=line[4])
+                        
+                        # Set the default value for the field
+                        
+                        if bool(line[6]):
+                            defaultValue = line[6]
+                            castedDefault = castValue(fieldType, defaultValue)
+                            arcpy.AssignDefaultToField_management(featureClass, line[0], castedDefault)
+                                     
+    except Exception as err: 
+        arcpy.AddError(traceback.format_exception_only(type(err), err)[0].rstrip())
+        
+    finally:
+        arcpy.AddMessage("Success! - Stopping: AddFieldsFromSchema")
+
+if __name__ == '__main__':
+    addFieldsFromSchema()
+        
---
+++ data/mil2525d/utilities/military-features-utilities/template-gdb-toolbox/scripts/CreateTemplateGDB/CreateTemplateGDB.py
@@ -0,0 +1,201 @@
+# ----------------------------------------------------------------------------------
+# Copyright 2013-2015 Esri
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ----------------------------------------------------------------------------------
+# CreateTemplateGDB.py
+#
+# Description: Creates an empty file geodatabase from the contents of
+# an input CSV file.  
+#
+# Adds a feature dataset and feature classes based on a supplied schema.  
+#
+# Adds fields to all those feature classes based on their supplied schemas.
+#
+# Requirements: ArcGIS Desktop
+# ----------------------------------------------------------------------------------
+
+import arcpy, os, sys, traceback
+import AddFieldsFromSchema
+import csv as csv
+import time
+
+from arcpy import env
+from os import path
+
+def geometryTypeLookup(jmsmlGeometryType):
+    
+    # Converts a JMSML symbol type into an Esri GIS geometry type
+    
+    switcher = {
+                "Point": "POINT",
+                "Line": "POLYLINE",
+                "Area": "POLYGON",
+                }
+    return switcher.get(jmsmlGeometryType, "NONE")
+
+def createVersionsTable(versionFile, geodatabase):
+    
+    # Creates a version table using the contents of the specified CSV file
+    # and today's date.  Inserts the table into the specified geodatabase
+    
+    try:
+        arcpy.AddMessage("Starting: CreateVersionsTable")
+        
+        currentPath = os.path.dirname(__file__)
+        defaultDataPath = os.path.normpath(os.path.join(currentPath, r'../../tooldata/'))
+        
+        if arcpy.Exists(versionFile):
+            # Set the current date as the creation date
+            
+            outFile = os.path.join(defaultDataPath, "temp.csv")
+            csvOut = open(outFile, "w")
+            writer = csv.writer(csvOut, delimiter=',', lineterminator='\n')
+            
+            with open(versionFile, "r") as csvIn:
+                reader = csv.reader(csvIn, delimiter=',', lineterminator='\n')
+                
+                for line in reader:
+                    if line[0] == "automated_creation_date" or line[0] == "last_modification_date":
+                        outString = time.strftime("%x")
+                    else:
+                        outString = line[1]
+                        
+                    writer.writerow([line[0], outString])
+                    
+                csvOut.close()         
+                    
+            # Now delete the original versions file and replace it with the new
+            
+            os.remove(versionFile)
+            os.rename(outFile, versionFile)
+            
+            # Now create the Versions table in the gdb
+            
+            tableName = "Versions"
+            
+            arcpy.CreateTable_management(geodatabase, tableName)
+            
+            table = os.path.join(geodatabase, tableName)
+            arcpy.AddField_management(table, "Item", "TEXT", field_length=50)
+            arcpy.AddField_management(table, "Version", "TEXT", field_length=50)
+            
+            arcpy.CopyRows_management(versionFile, table)
+    except Exception as err: 
+        arcpy.AddError(traceback.format_exception_only(type(err), err)[0].rstrip())
+        
+    finally:
+        arcpy.AddMessage("Success! - Stopping: CreateVersionsTable")
+            
+def createTemplateGDB(workspace="GET_PARAMETER", version="GET_PARAMETER"):
+    
+    # Creates the specified file geodatabase of the specified version
+    
+    try:
+        arcpy.AddMessage("Starting: CreateEmptyGDB")
+        
+        currentPath = os.path.dirname(__file__)
+        defaultDataPath = os.path.normpath(os.path.join(currentPath, r'../../tooldata/'))
+        
+        # Create a new file gdb in supplied folder  
+               
+        if workspace == "GET_PARAMETER":
+            destinationFolder = arcpy.GetParameterAsText(0)
+            
+        if version == "GET_PARAMETER":   
+            version = arcpy.GetParameterAsText(1)
+        
+        # Schema details are read from CSV files created by JMSML
+        
+        # Open the schemas CSV file which contains the basic structure of the gdb
+        # we are going to create
+        
+        schemaFile = os.path.join(defaultDataPath, "Schemas.csv")
+        
+        with open(schemaFile, 'r') as csvFile:
+            reader = csv.reader(csvFile, dialect='excel')
+            
+            # Skip the headers
+            header = next(reader)
+            
+            # Read the second line, the line of data should be a geodatabase schema line
+            
+            line = next(reader)
+            
+            if line[0] == "SchemaContainer":
+                arcpy.AddMessage("Creating file geodatabase " + line[1] + "...")
+                
+                gdbName = line[1] + ".gdb"
+                gdbPath = os.path.join(destinationFolder, gdbName)
+                
+                arcpy.env.workspace = destinationFolder
+  
+                if(arcpy.Exists(gdbName)):
+                    arcpy.Delete_management(gdbName)
+           
+                arcpy.CreateFileGDB_management(destinationFolder, gdbName, version)
+                
+                # Create a Versions table from the contents of a CSV file
+                
+                createVersionsTable(os.path.join(defaultDataPath, "Versions.csv"), gdbPath)
+                        
+                # Read the next line of data.  It should be a dataset and its metadata
+                
+                line = next(reader)
+                
+                if line[0] == "SchemaSet":
+                    arcpy.AddMessage("Creating feature dataset " + line[1] + "...")
+                    
+                    # Fetch the dataset's spatial reference
+                    
+                    dataSet = line[1]
+                    sr = arcpy.SpatialReference(int(line[17]))
+                    
+                    arcpy.CreateFeatureDataset_management(gdbPath, dataSet, spatial_reference=sr)
+                    
+                    # Read the following lines of the CSV and create a feature class for each
+                    
+                    for line in reader:
+                        if line[0] == "Schema":
+                            fcName = line[1]
+                            geoType = line[2]
+                            alias = line[3]
+                            srID = line[17]
+                            
+                            arcpy.AddMessage("Creating feature class " + fcName + "...")
+                            
+                            geometryType = geometryTypeLookup(geoType)
+                            
+                            if geometryType != "NONE":
+                                out_path = os.path.join(gdbPath, dataSet)
+                                
+                                if srID == "":
+                                    arcpy.CreateFeatureclass_management(out_path, fcName, geometryType, has_m="DISABLED", has_z="ENABLED")
+                                else:
+                                    sr = arcpy.SpatialReference(int(srID))
+                                    arcpy.CreateFeatureclass_management(out_path, fcName, geometryType, spatial_reference=sr, has_m="DISABLED", has_z="ENABLED")
+                                
+                                featureClass = os.path.join(out_path, fcName)
+                                arcpy.AlterAliasName(featureClass, alias)
+                                
+                                AddFieldsFromSchema.addFieldsFromSchema(featureClass, fcName)
+                        
+        
+    except Exception as err: 
+        arcpy.AddError(traceback.format_exception_only(type(err), err)[0].rstrip())
+        
+    finally:
+        arcpy.AddMessage("Success! - Stopping: CreateEmptyGDB")
+        
+if __name__ == '__main__':
+    createTemplateGDB()
+    
