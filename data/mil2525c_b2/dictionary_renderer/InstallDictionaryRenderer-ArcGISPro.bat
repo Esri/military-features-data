8030b5fd842f4bdcd3ab86681836fd1753ba91b7
Chris Moore <csmooreacm@acm.org>
2016-06-24 12:37:25 -0400

    Add Legacy Dictionary Renderers and Test Projects
    Also updated mil2525d.stylx and added the source csvs used to generate
    the styles/schemas so we can better track these changes

---
+++ data/mil2525c_b2/dictionary_renderer/InstallDictionaryRenderer-ArcGISPro.bat
@@ -0,0 +1,33 @@
+REM Change back to current directory
+pushd %~dp0 
+
+SET PRO_DIR=C:\Program Files\ArcGIS\Pro
+IF NOT EXIST "%PRO_DIR%" SET PRO_DIR=%LOCALAPPDATA%\Programs\ArcGIS\Pro
+
+IF EXIST "%PRO_DIR%" goto pro_exists_ok
+
+echo Can't find Pro install at: %PRO_DIR% - can't continue
+pause
+goto :EOF
+
+:pro_exists_ok
+
+SET DictionaryRenderer=mil2525c_b2
+IF NOT EXIST %DictionaryRenderer% goto folder_error
+xcopy /v /y /s %DictionaryRenderer% "%PRO_DIR%\Resources\Dictionaries"
+
+echo off
+echo ********************************
+echo * ArcGIS Pro Dictionary Added: %DictionaryRenderer%
+echo ********************************
+pause
+goto :EOF
+
+:folder_error
+
+echo off
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+echo ! FAILED:
+echo ! Could not find Folder: %DictionaryRenderer%
+echo !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+pause
