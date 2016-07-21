466b8679b3653d31cfa441310790fb234df74f1b
Daniel Barnes <Daniel_Barnes@esri.com>
2016-07-21 14:46:44 -0700

    Updating Complete Test Datasets + Scripts and Models for C_Bc2
    These complete test datasets show every symbol for each appendix that
    can be rendered with our legacy renderer using the C or Bc2 setting. In
    addition, included are a script and a toolbox. The python script allows
    for users to filter out source CSVs to get specific appendices, whereas
    the models in the toolbox create the test data and match it up to the
    specific CSVs, giving an editable test dataset as an output. In
    addition, included in this PR is a list of control measures that don't
    follow the point ordering dictated by 2525D, in order to increase
    transparency for users of the Military Overlay template.

---
+++ data/mil2525c_b2/test_data/Complete_test_scripts/FilterCSVMilSymbols.py
@@ -0,0 +1,73 @@
+#import CSV Module
+import csv
+#import arcpy
+import arcpy
+
+#filtergeom = 'POINT'
+#filterappendix = 'A'
+inputFileName = arcpy.GetParameterAsText(0)
+outputfolder = arcpy.GetParameterAsText(1)
+
+outputFileDict = {"AppendixA_Points.csv": ["A", "POINT"], \
+                  "AppendixB_Points.csv": ["B", "POINT"], \
+                  "AppendixB_lines.csv": ["B", "LINE"], \
+                  "AppendixB_areas.csv": ["B", "AREA"], \
+                  "AppendixC_Points.csv": ["C", "POINT"], \
+                  "AppendixC_lines.csv": ["C", "LINE"], \
+                  "AppendixC_areas.csv": ["C", "AREA"], \
+                  "AppendixD_Points.csv": ["D", "POINT"], \
+                  "AppendixE_Points.csv": ["E", "POINT"], \
+                  "AppendixG_Points.csv": ["G", "POINT"]}
+
+for key in outputFileDict:
+    dictionaryEntry = outputFileDict[key]
+    filterappendix = dictionaryEntry[0]
+    filtergeom = dictionaryEntry[1]
+    print(key, outputFileDict[key], filterappendix, filtergeom)
+
+    if outputfolder == "" or outputfolder is None:
+        outputfolder = "C:\\Users\\dani8200\\Documents\\MilitarySymbology\\Test Datasets\\PythonforTestDatasets\\Cresults"
+    if inputFileName =="" or outputfolder is None:
+        inputFileName = outputfolder + "Mil2525C.csv"
+
+    inputfile = open(inputFileName, "r")
+    reader = csv.reader(inputfile)
+
+    filterOutputFileName = key
+    outputFileName = outputfolder + "\\" + filterOutputFileName
+    outputfile = open(outputFileName, 'w')
+    writer = csv.writer(outputfile)
+
+    print("Opening " + inputFileName)
+    arcpy.AddMessage("Opening " + inputFileName)
+    print("Writing " + outputFileName)
+    arcpy.AddMessage("Writing " + outputFileName)
+
+    inputRowCount = 0
+    outputRowCount = 0
+
+    for row in reader:
+        columns = row
+        inputRowCount += 1
+
+        outputrow = None
+        if len(columns) > 8:
+            OID = columns[0]
+            Appendix = columns[1]
+            FullSIDC = columns[3]
+            SIDCbyParts = columns[4]
+            HierarchyCode = columns[5]
+            Name = columns[6]
+            Geometry = columns[7]
+            Notes = columns [8]
+            if ((Geometry == filtergeom) and (Appendix == filterappendix)) or (inputRowCount == 1):
+                outputOID = str(outputRowCount)
+                if outputRowCount == 0:
+                    outputOID = "OID"
+                outputrow = [outputOID, Appendix, FullSIDC, SIDCbyParts, HierarchyCode, Name, Geometry, Notes]
+                print(outputrow)
+                writer.writerow(outputrow)
+                outputRowCount +=1
+
+    arcpy.AddMessage("Done Processing " + outputFileName + " Lines Read = " + str(inputRowCount) + " Lines Found = " + str(outputRowCount))
+
